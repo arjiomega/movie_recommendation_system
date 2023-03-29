4f63ea4f59409b3958607bfe216ad1e73dad78ec
Richard Omega <richardjoseph.omega@gmail.com>
2023-03-29 14:29:43 +0800

    aws cicd v0.18 (aws build dir fix 5)

--- scripts/generate_vars.sh
+++ scripts/generate_vars.sh
@@ -17,7 +17,7 @@ PARAM_NAMES=(
 # Loop over the parameter names and retrieve the values from AWS Parameter Store
 for PARAM_NAME in "${PARAM_NAMES[@]}"
 do
-    PARAM_VALUE=$(aws ssm get-parameters --region ap-northeast-1 --names "${PARAM_NAME}" --query Parameters[0].Value --output text)
+    PARAM_VALUE=$(aws ssm get-parameters --region ap-northeast-1 --names "${PARAM_NAME}" --with-decryption --query Parameters[0].Value)
     echo ${PARAM_NAME}=\""${PARAM_VALUE}"\"
     echo ${PARAM_NAME}=\""${PARAM_VALUE}"\" >> /home/ubuntu/django_project/config.py
 done
