ebdca5ceb6e66ce57c28a05787e0ea8a7f04efa8
Richard Omega <richardjoseph.omega@gmail.com>
2023-03-29 15:29:11 +0800

    aws cicd v0.23 (writing permission 2)

--- scripts/generate_vars.sh
+++ scripts/generate_vars.sh
@@ -4,15 +4,15 @@ echo generate variables starting...
 
 # remove config.py if it exists
 echo remove config.py
-rm -f /home/ubuntu/django_project/config.py
+sudo rm -f /home/ubuntu/django_project/config.py
 
 # create config.py file
 echo generate blank config.py
-touch /home/ubuntu/django_project/config.py
+sudo touch /home/ubuntu/django_project/config.py
 
 # add permission
 echo add permission to config.py
-chmod 777 /home/ubuntu/django_project/config.py
+sudo chmod 777 /home/ubuntu/django_project/config.py
 
 # Array of parameter names
 PARAM_NAMES=(
@@ -31,8 +31,8 @@ echo Retrieve values from aws parameter store
 for PARAM_NAME in "${PARAM_NAMES[@]}"
 do
     echo ${PARAM_NAME}
-    PARAM_VALUE=$(aws ssm get-parameters --region ap-northeast-1 --names "${PARAM_NAME}" --with-decryption --query Parameters[0].Value)
+    PARAM_VALUE=$(aws ssm get-parameters --region ap-northeast-1 --names "${PARAM_NAME}" --with-decryption --query Parameters[0].Value --output text)
     echo ${PARAM_VALUE}
     echo ${PARAM_NAME}=\""${PARAM_VALUE}"\"
-    echo ${PARAM_NAME}=\""${PARAM_VALUE}"\" >> /home/ubuntu/django_project/config.py
+    sudo echo ${PARAM_NAME}=\""${PARAM_VALUE}"\" >> /home/ubuntu/django_project/config.py
 done
