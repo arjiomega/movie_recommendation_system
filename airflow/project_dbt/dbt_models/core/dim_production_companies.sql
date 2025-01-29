24fb62866fdc53766ede92d00a307b359bf32ae6
Richard Joseph Omega <richardjoseph.omega@gmail.com>
2025-01-29 21:03:06 +0800

    prevent duplicates in production companies

--- airflow/project_dbt/dbt_models/core/dim_production_companies.sql
+++ airflow/project_dbt/dbt_models/core/dim_production_companies.sql
@@ -12,12 +12,28 @@ WITH movies_in_increment as (
             mov.release_date >= '{{ var("start_date") }}' AND -- 2020-01-01
             mov.release_date < '{{ var("end_date") }}' -- 2020-02-01
     {% endif %}
+),
+ranked_companies AS (
+    SELECT
+        production_company_element ->> 'id' AS company_id,
+        production_company_element ->> 'origin_country' AS country_id,
+        production_company_element ->> 'name' AS company_name,
+        production_company_element ->> 'logo_path' AS logo_path,
+        ROW_NUMBER() OVER (
+            PARTITION BY production_company_element ->> 'id'
+            ORDER BY 
+                CASE 
+                    WHEN production_company_element ->> 'logo_path' IS NOT NULL THEN 1 
+                    ELSE 2 
+                END ASC
+        ) AS rank
+    FROM {{ source('staging', 'raw_movies') }} mov,
+         LATERAL JSONB_ARRAY_ELEMENTS(mov.production_companies) AS production_company_element
+    WHERE mov.id IN (SELECT movie_id FROM movies_in_increment)
 )
-SELECT DISTINCT
-    production_company_element ->> 'id' AS company_id,
-    production_company_element ->> 'origin_country' AS country_id,
-    production_company_element ->> 'name' AS company_name,
-    production_company_element ->> 'logo_path' AS logo_path
-FROM {{ source('staging', 'raw_movies')  }} mov,
-     LATERAL JSONB_ARRAY_ELEMENTS(mov.production_companies) AS production_company_element
-WHERE mov.id IN (SELECT movie_id FROM movies_in_increment)
+SELECT 
+    company_id,
+    company_name,
+    logo_path
+FROM ranked_companies
+WHERE rank = 1
